6e8d5dfa6973f93f71bc6889e10770723c406607
kimsooyoung <tge1375@naver.com>
2025-05-29 03:41:42 -0600

    problem 4 updated

--- project3/proj3_problem4.m
+++ project3/proj3_problem4.m
@@ -1,52 +1,47 @@
 clc; clear; close all;
 
 % Parameters
-N_list = [5, 10];           % FIR filter lengths to try
-delta_list = [0.01, 0.05, 0.1, 0.2, 0.5, 0.7, 1.0, 2.0, 4.0];  % Step sizes
-num_samples = 3000;         % Total number of samples
+N_values = [5, 10];           % FIR filter lengths
+delta_values = [0.1, 0.5, 1.0];  % LMS step sizes
+K_factor = 10;                % Multiplier for averaging interval
+M = 3000;                     % Total number of samples
 
 % Generate input signal x(n)
-x = 2*rand(1, num_samples) - 1;
+x = 2*rand(1, M) - 1;
 
 % Define unknown system
 a = [1 -0.7 0.12];
 b = [1 1.5 0.56];
-d = filter(b, a, x);  % Output of unknown system
+d = filter(b, a, x);  % Desired output
 
-% Loop over FIR lengths
-for N_idx = 1:length(N_list)
-    N = N_list(N_idx);
-    K = 10 * N;  % Averaging interval
+figure; % initialize plot 1
+for i = 1:length(N_values)
+    N = N_values(i);          % Select FIR filter length
+    K = K_factor * N;         % Averaging interval
 
-    figure;
-    hold on;
-    legend_entries = {};
-    
-    % Loop over step sizes
-    for delta_idx = 1:length(delta_list)
-        delta1 = delta_list(delta_idx);
-        
-        % Apply LMS
-        [~, y] = lms(x, d, delta1, N);
+    for j = 1:length(delta_values)
+        delta = delta_values(j);  % LMS step size
+
+        % Run LMS algorithm
+        [~, y] = lms(x, d, delta, N);
+
+        % Compute error signal
         e = d - y;
-        
-        % Compute ASE(m)
-        m_values = 0:K:(num_samples - K);
-        ASE = zeros(1, length(m_values));
-        for m_idx = 1:length(m_values)
-            n = m_values(m_idx);
-            ASE(m_idx) = mean(e(n+1:n+K).^2);
+
+        % Compute ASE over segments of size K
+        num_segments = floor((M - N) / K);
+        ASE = zeros(1, num_segments);
+        for m = 1:num_segments
+            idx_start = (m-1)*K + N + 1;
+            idx_end = idx_start + K - 1;
+            ASE(m) = mean(e(idx_start:idx_end).^2);
         end
 
-        % Plot ASE(m)
-        plot(m_values, ASE, 'DisplayName', ['\Delta = ', num2str(delta1)]);
-        legend_entries{end+1} = ['\Delta = ', num2str(delta1)];
+        % Plot ASE for current configuration
+        subplot(length(N_values), length(delta_values), (i-1)*length(delta_values)+j);
+        plot(ASE, 'LineWidth', 1.5);
+        xlabel('m'); ylabel('ASE(m)');
+        title(sprintf('N = %d, \\Delta = %.2f', N, delta));
+        grid on;
     end
-
-    hold off;
-    xlabel('Sample index n');
-    ylabel('ASE(m)');
-    title(['ASE vs n for FIR filter length N = ', num2str(N)]);
-    legend('show');
-    grid on;
 end
